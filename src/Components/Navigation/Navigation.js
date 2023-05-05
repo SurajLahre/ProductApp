d4d39077c86b627039c7c1ba74121ceec5da0a64
SurajLahre <107786386+SurajLahre@users.noreply.github.com>
2023-05-05 10:10:07 +0530

    done

---
+++ src/Components/Navigation/Navigation.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+function Navigation() {
+    return (
+        <>
+            <div className="container">
+                <h1>Hello i am Navigation section</h1>
+            </div>
+        </>
+    )
+}
+
+export default Navigation
